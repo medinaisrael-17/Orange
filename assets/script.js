90fd9c6f06536f8ba59a0ec6787d0d0eb4d24548
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-04-28 09:37:40 -0500

    two more sections and animation on the albums

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,23 @@ const poemScene = new ScrollMagic.Scene({
 })
     .setTween(poemTween)
     .addTo(controller)
+
+const creativityTween = new TweenMax.to("#text-box-2 h1", 1, {
+    top: 0
+})
+
+const creativityScene = new ScrollMagic.Scene({
+    triggerElement: "#section-2",
+    duration: 300
+})
+    .setTween(creativityTween)
+    .addTo(controller);
+
+const albumScene = new ScrollMagic.Scene({
+    triggerElement: "#section-3",
+    duration: 50,
+    reverse: false
+})
+    .setClassToggle(".album", "bounceIn")
+    .addIndicators()
+    .addTo(controller); 
